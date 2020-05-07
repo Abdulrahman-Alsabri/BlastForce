52f62f75c36b2b980e2f16ce25d97355cfbaf3cc
Abdulrahman51 <Aalsabri2017@my.fit.edu>
2020-05-07 13:30:01 +0400

    Updated All Scripts

---
+++ 4_Code/CollectedCoins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectedCoins : MonoBehaviour
+{
+    Score score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // searches for score
+        GameObject temp2 = GameObject.Find("Score");
+        if (temp2 != null)
+        {
+            // gets score
+            score = temp2.GetComponent<Score>();
+        }
+        else
+        {
+            Debug.Log("Score not found");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            score.addToCoins();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 4_Code/HubTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubTrigger : MonoBehaviour
+{
+    // reference scene changer script
+    SceneChanger changer;
+
+    // initialize time needed between switching scenes
+    private int timeToWait = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // searches for SceneChanger
+        GameObject temp = GameObject.Find("SceneChanger");
+        if (temp != null)
+        {
+            // gets SceneChanger
+            changer = temp.GetComponent<SceneChanger>();
+        }
+        else
+        {
+            Debug.Log("SceneChanger not found");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // face out and wait
+            changer.FadeToScene(10);
+            StartCoroutine(WaitAndLoad(timeToWait));
+        }
+    }
+
+    IEnumerator WaitAndLoad(int waitingTime)
+    {
+        yield return new WaitForSeconds(waitingTime);
+    }
+}
